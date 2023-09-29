5d96281774954539ab120c62ad46cd81df1f4b00
AnandavdM <143999146+AnandavdM@users.noreply.github.com>
2023-09-29 15:59:05 +0200

    Add files via upload

--- basiswebsite/scripts/script.js
+++ basiswebsite/scripts/script.js
@@ -26,23 +26,23 @@ var video = document.getElementById("background-video");
 
 // LETTERGROOTTE//
 
-// Functie om de lettergrootte te vergroten
-    function increaseFontSize() {
-        var currentSize = parseFloat(getComputedStyle(document.body).fontSize);
-        var newSize = currentSize + 2; // Pas de toename aan
-        document.body.style.fontSize = newSize + "px";
-    }
+// // Functie om de lettergrootte te vergroten
+//     function increaseFontSize() {
+//         var currentSize = parseFloat(getComputedStyle(document.body).fontSize);
+//         var newSize = currentSize + 2; // Pas de toename aan
+//         document.body.style.fontSize = newSize + "px";
+//     }
 
-    // Functie om de lettergrootte te verkleinen
-    function decreaseFontSize() {
-        var currentSize = parseFloat(getComputedStyle(document.body).fontSize);
-        var newSize = currentSize - 2; // Pas de vermindering aan
-        document.body.style.fontSize = newSize + "px";
-    }
+//     // Functie om de lettergrootte te verkleinen
+//     function decreaseFontSize() {
+//         var currentSize = parseFloat(getComputedStyle(document.body).fontSize);
+//         var newSize = currentSize - 2; // Pas de vermindering aan
+//         document.body.style.fontSize = newSize + "px";
+//     }
 
-    // Voeg eventlisteners toe aan de knoppen
-    var increaseButton = document.getElementById("increase-font-size");
-    increaseButton.addEventListener("click", increaseFontSize);
+//     // Voeg eventlisteners toe aan de knoppen
+//     var increaseButton = document.getElementById("increase-font-size");
+//     increaseButton.addEventListener("click", increaseFontSize);
 
-    var decreaseButton = document.getElementById("decrease-font-size");
-    decreaseButton.addEventListener("click", decreaseFontSize);
+//     var decreaseButton = document.getElementById("decrease-font-size");
+//     decreaseButton.addEventListener("click", decreaseFontSize);
